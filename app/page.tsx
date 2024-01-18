6e90d5267b64e8307af35440d05352e10f359172
Olivér Angyal <oliver.angyal@gmail.com>
2024-01-19 00:33:43 +0100

    💄 add navbar

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import Header from "@/components/Header";
 import { cookies } from "next/headers";
 import { ModeToggle } from "@/components/ui/mode-toggle";
 
+import { MainNav } from "@/components/MainNav";
+
 export default async function Index() {
   const cookieStore = cookies();
 
@@ -19,14 +21,19 @@ export default async function Index() {
   const isSupabaseConnected = canInitSupabaseClient();
 
   return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-        <div className="w-full max-w-4xl flex justify-right items-center p-3 text-sm">
-          <div className="ml-auto">{isSupabaseConnected && <AuthButton />}</div>
-          <ModeToggle />
+    <div className="w-full flex flex-col gap-20 items-center">
+      <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+        <div className="flex h-14 max-w-screen-2xl items-center px-8">
+          <div className="w-[13%]">
+            <i>q</i>
+          </div>
+          <MainNav />
+          <div className="flex ml-auto">
+            <div className="mr-4">{isSupabaseConnected && <AuthButton />}</div>
+            <ModeToggle />
+          </div>
         </div>
-      </nav>
-
+      </header>
       <div className="animate-in flex-1 flex flex-col gap-20 max-w-4xl px-3">
         <Header />
       </div>
