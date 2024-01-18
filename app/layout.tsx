99dcd043e08208f45950faafa5f1b31af88ce7a2
Olivér Angyal <oliver.angyal@gmail.com>
2024-01-18 21:18:00 +0100

    🗑️ clean up code

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "quizlet, but it's free",
+  description: "the most inexpensive way to learn from flashcards",
 };
 
 export default function RootLayout({
